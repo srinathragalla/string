a8683c7851c63bad379c67373bd0f472ee88a838
srinath ragalla <srinathragalla17@gmail.com>
2021-12-14 12:25:59 +0530

    string

---
+++ string.js
@@ -0,0 +1,9 @@
+var myStr="creating a string in \"javascript\""
+console.log(myStr);
+
+var myStr="creating a string in \'javascript\'"
+console.log(myStr);
+
+var myStr="just a string"
+console.log(myStr);
+
